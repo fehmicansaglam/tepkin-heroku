43bd2867b6a09ebdc3f4141782e075ddf0e4f919
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2015-03-31 21:49:39 +0300

    Update libraries

--- build.sbt
+++ build.sbt
@@ -2,13 +2,33 @@ import NativePackagerKeys._
 
 packageArchetype.java_application
 
-name := """scala-getting-started"""
+name := """tepkin"""
 
 version := "1.0"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.6"
+
+scalacOptions := Seq(
+  "-deprecation",
+  "-encoding", "UTF-8", // yes, this is 2 args
+  "-feature",
+  "-language:existentials",
+  "-language:higherKinds",
+  "-language:implicitConversions",
+  "-unchecked",
+  "-Xfatal-warnings",
+  "-Xlint",
+  "-Yno-adapted-args",
+  "-Ywarn-dead-code", // N.B. doesn't work well with the ??? hole
+  "-Ywarn-numeric-widen",
+  "-Ywarn-value-discard",
+  "-Xfuture",
+  "-Ywarn-unused-import" // 2.11 only
+)
   
+resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
 libraryDependencies ++= Seq(
-  "com.twitter" % "finagle-http_2.10" % "6.18.0",
-  "postgresql" % "postgresql" % "9.0-801.jdbc4"
+  "com.twitter" %% "finagle-http" % "6.24.0",
+  "net.fehmicansaglam" %% "tepkin" % "0.2-SNAPSHOT"
 )
--- src/main/scala/com/example/Server.scala
+++ src/main/scala/com/example/Server.scala
@@ -1,69 +1,79 @@
 package com.example
 
-import java.net.URI
-import java.sql.{Connection, DriverManager}
-
+import akka.stream.ActorFlowMaterializer
+import akka.util.Timeout
 import com.twitter.finagle.http.Response
 import com.twitter.finagle.{Http, Service}
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Await, Future => TwitterFuture, Promise => TwitterPromise}
+import net.fehmicansaglam.bson.BsonDocument
+import net.fehmicansaglam.bson.BsonDsl._
+import net.fehmicansaglam.bson.Implicits._
+import net.fehmicansaglam.tepkin.MongoClient
 import org.jboss.netty.handler.codec.http._
+import org.joda.time.DateTime
 
-import scala.util.Properties
+import scala.concurrent.duration._
+import scala.concurrent.{Future => ScalaFuture}
+import scala.util.{Failure, Properties, Success}
 
 object Server {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     val port = Properties.envOrElse("PORT", "8080").toInt
-    println("Starting on port: "+port)
+    println("Starting on port: " + port)
 
     val server = Http.serve(":" + port, new Hello)
     Await.ready(server)
+    ()
   }
 }
 
 class Hello extends Service[HttpRequest, HttpResponse] {
-  def apply(request: HttpRequest): Future[HttpResponse] = {
+  val uri = Properties.envOrElse("MONGOLAB_URI", "")
+  val client = MongoClient(uri)
+  val db = client("tepkin")
+  val collection = db("tepkin")
+
+  import client.ec
+
+  implicit val timeout: Timeout = 5.seconds
+  implicit val mat = ActorFlowMaterializer()(client.context)
+
+  implicit def toTwitterFuture[A](sf: ScalaFuture[A]): TwitterFuture[A] = {
+    val tp = new TwitterPromise[A]
+
+    sf.onComplete {
+      case Success(v) => tp.setValue(v)
+      case Failure(t) => tp.setException(t)
+    }
+
+    tp
+  }
+
+  def apply(request: HttpRequest): TwitterFuture[HttpResponse] = {
     if (request.getUri.endsWith("/db")) {
-      showDatabase(request);
+      showDatabase(request)
     } else {
-      showHome(request);
+      showHome(request)
     }
   }
 
-  def showHome(request: HttpRequest): Future[HttpResponse] = {
+  def showHome(request: HttpRequest): TwitterFuture[HttpResponse] = {
     val response = Response()
     response.setStatusCode(200)
     response.setContentString("Hello from Scala!")
-    Future(response)
+    TwitterFuture(response)
   }
 
-  def showDatabase(request: HttpRequest): Future[HttpResponse] = {
-    val connection = getConnection
-    try {
-      val stmt = connection.createStatement
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      var out = ""
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-
+  def showDatabase(request: HttpRequest): TwitterFuture[HttpResponse] = {
+    for {
+      insert <- collection.insert("created" := DateTime.now())
+      source <- collection.find(BsonDocument.empty)
+      out <- source.runFold("")(_ + _.map(_.pretty()).mkString("\n"))
+    } yield {
       val response = Response()
       response.setStatusCode(200)
       response.setContentString(out)
-      Future(response)
-    } finally {
-      connection.close()
+      response.httpResponse
     }
-  }
-
-  def getConnection(): Connection = {
-    val dbUri = new URI(System.getenv("DATABASE_URL"))
-    val username = dbUri.getUserInfo.split(":")(0)
-    val password = dbUri.getUserInfo.split(":")(1)
-    val dbUrl = s"jdbc:postgresql://${dbUri.getHost}:${dbUri.getPort}${dbUri.getPath}"
-    DriverManager.getConnection(dbUrl, username, password)
   }
 }
